eca3548baca2ba4fbed2addf960342d846936b39
Havard Graff <havard.graff@gmail.com>
2013-03-21 17:49:18 -0400

    rtp-stream: plug session leak

--- gst/fsrtpconference/fs-rtp-stream.c
+++ gst/fsrtpconference/fs-rtp-stream.c
@@ -1083,6 +1083,7 @@ fs_rtp_stream_set_transmitter (FsStream *stream,
   if (self->priv->stream_transmitter)
   {
     FS_RTP_SESSION_UNLOCK (session);
+    g_object_unref (session);
     return FALSE;
   }
   FS_RTP_SESSION_UNLOCK (session);
@@ -1149,8 +1150,10 @@ fs_rtp_stream_set_transmitter (FsStream *stream,
     self->priv->stream_transmitter = NULL;
     FS_RTP_SESSION_UNLOCK (session);
     g_object_unref (st);
+    g_object_unref (session);
     return FALSE;
   }
 
+  g_object_unref (session);
   return TRUE;
 }
